5c36c982766e300f4e37de348cd5215dafb1636c
Behroz Karim <behrozbuzdar@gmail.com>
2022-06-20 15:27:27 +0500

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,20 +3,21 @@ class Product < ApplicationRecord
     validates :price, numericality: { greater_than: 0 }
     validates :quantity, numericality: { greater_than: 0 }
     validates :name, length: { minimum: 2 }
-    # after_create :after_function
-    # after_save :after_function, if: :saved_change_to_name?
-    # after_commit :after_commit_function
-    # before_commit :before_commit_function
-    # private
-    # def after_function
-    #     puts "Name of Product is #{self.name}"*100
-    # end
+    after_create :after_function
+    after_save :after_function, if: :saved_change_to_name?
+    after_commit :after_commit_function
+    before_commit :before_commit_function
+    
+    private
+    def after_function
+        puts "Name of Product is #{self.name}"*100
+    end
 
-    # def after_commit_function
-    #     puts "After Commit \n"*100
-    # end
+    def after_commit_function
+        puts "After Commit \n"*100
+    end
 
-    # def before_commit_function
-    #     puts "before Commit \n"*100
-    # end
+    def before_commit_function
+        puts "before Commit \n"*100
+    end
 end
